3142cc51252807b66870eeea565e3c5473261972
emrefiliz01 <98563077+emrefiliz01@users.noreply.github.com>
2025-07-08 18:35:15 +0300

    CameraController added

--- Assets/02_Dev/Scripts/Camera/CameraController.cs
+++ Assets/02_Dev/Scripts/Camera/CameraController.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
-{  
-   
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Vector3 offset = new Vector3(0, 0, 0);
+
+    private void Update()
+    {
+        transform.position = player.position + offset;
+    }   
 }
