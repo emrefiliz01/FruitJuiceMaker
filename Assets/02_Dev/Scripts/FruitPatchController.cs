fb69d6ae8867c949462acf33d944e15145eb9154
emrefiliz01 <98563077+emrefiliz01@users.noreply.github.com>
2025-07-10 19:57:35 +0300

    ewww

--- Assets/02_Dev/Scripts/FruitPatchController.cs
+++ Assets/02_Dev/Scripts/FruitPatchController.cs
@@ -11,50 +11,73 @@ public class FruitPatchController : MonoBehaviour
 
     private int currentStage;
     private float currentTimer;
+    private Coroutine startTimerCoroutine;
 
     private void Start()
     {
         currentStage = 1;
 
         StageReset();
+
+        startTimerCoroutine = StartCoroutine(StartTimerCoroutine());
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-           // StageReset();
-            currentStage = 0;
-        }
-
-
-        if (currentTimer > 0 && currentStage != stageList.Count)
-        {
-            currentTimer -= Time.deltaTime;
-        }
-        else
-        {
-            currentTimer = 0;
-            IncreaseFruitStage();
-        }
-
-        timerText.text = currentTimer.ToString("F0");
-    }
-
-    private void IncreaseFruitStage()
-    {
-        if (currentStage >= stageList.Count)
-        {
-            currentTimer = 0;
-        }
-        else
-        {
-            currentStage++;
-
+            currentStage = 1;
             StageReset();
         }
 
+
+      //  if (currentTimer > 0 && currentStage != stageList.Count)
+      // {
+      //      currentTimer -= Time.deltaTime;
+      //  }
+      //  else
+      //  {
+       //     currentTimer = 0;
+         //   IncreaseFruitStage();
+        //}
+
+        timerText.text = currentTimer.ToString("F0");
+    }
+    
+    private void IncreaseFruitStage()
+    {
+        Debug.Log(currentStage);
+
+        currentStage += 1;
+
+        if (currentStage <= stageList.Count && currentStage != stageList.Count)
+        {
+            StageReset();
+        }
+        else
+        {
+            StageReset();
+
+            StopCoroutine(startTimerCoroutine);
+
+            timerText.gameObject.SetActive(false);
+        }
+    }
+    private IEnumerator StartTimerCoroutine()
+    {
+        while (currentStage != stageList.Count)
+        {
+            yield return new WaitForSeconds(1f);
+
+            currentTimer -= 1;
+
+            if (currentTimer <= 0)
+            {
+                IncreaseFruitStage();
+            }
+        }
         
+        yield return null;
     }
 
     private void StageReset()
@@ -67,5 +90,6 @@ public class FruitPatchController : MonoBehaviour
         stageList[currentStage - 1].SetActive(true);
 
         currentTimer = fruitPatchSO.stageTimer;
+        timerText.gameObject.SetActive(true);
     }
 }
