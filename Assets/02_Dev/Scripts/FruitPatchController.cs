4d76c0f685a3d6b916e3a905bad7263c6f9634d2
emrefiliz01 <98563077+emrefiliz01@users.noreply.github.com>
2025-07-10 12:46:41 +0300

    added timer

--- Assets/02_Dev/Scripts/FruitPatchController.cs
+++ Assets/02_Dev/Scripts/FruitPatchController.cs
@@ -1,24 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class FruitPatchController : MonoBehaviour
 {
     [SerializeField] private FruitPatchSO fruitPatchSO;
     [SerializeField] private List<GameObject> stageList;
+    [SerializeField] private TextMeshPro timerText;
 
-    int currentStage;
-    int currentTimer;
+    private int currentStage;
+    private float currentTimer;
 
     private void Start()
     {
         currentStage = 1;
 
+        StageReset();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+           // StageReset();
+            currentStage = 0;
+        }
+
+
+        if (currentTimer > 0)
+        {
+            currentTimer -= Time.deltaTime;
+        }
+        else
+        {
+            currentTimer = 0;
+            IncreaseFruitStage();
+        }
+
+        timerText.text = currentTimer.ToString("F0");
+    }
+
+    private void IncreaseFruitStage()
+    {
+        if (currentStage >= stageList.Count)
+        {
+            currentTimer = 0;
+        }
+        else
+        {
+            currentStage++;
+
+            StageReset();
+        }
+
+        
+    }
+
+    private void StageReset()
+    {
         foreach (GameObject stage in stageList)
         {
             stage.SetActive(false);
         }
 
-         stageList[currentStage - 1].SetActive(true);   
+        stageList[currentStage - 1].SetActive(true);
+
+        currentTimer = fruitPatchSO.stageTimer;
     }
 }
